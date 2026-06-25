ade20af5242b671f8b0f41a53a658b2fc2f945e6
sahapolvan <sahapon3837@gmail.com>
2026-06-25 21:42:26 +0700

    Update background.js

--- background.js
+++ background.js
@@ -1,13 +1,2 @@
-const bg = document.createElement('div');
-
-bg.style.position = 'fixed';
-bg.style.top = '0';
-bg.style.left = '0';
-bg.style.width = '100%';
-bg.style.height = '100%';
-bg.style.background =
-'linear-gradient(to bottom, #87CEEB, #F3E9D2)';
-
-bg.style.zIndex = '-1';
-
-document.body.prepend(bg);
+console.log("background.js loaded");
+document.body.style.background = "red";
