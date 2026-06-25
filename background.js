b587476fe3376e1a393d6e56cc60adb70447b481
sahapolvan <sahapon3837@gmail.com>
2026-06-25 18:57:05 +0700

    Create background.js

---
+++ background.js
@@ -0,0 +1,10 @@
+const bg = document.createElement("div");
+
+bg.innerHTML = `
+<div class="sky"></div>
+<div class="cloud"></div>
+<div class="rice-field"></div>
+<div class="house"></div>
+`;
+
+document.body.prepend(bg);
